8a14cc78a6797bf1e3c9aded54fded3924440f2c
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-03-14 17:29:55 -0700

    setup done and db created

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-let express = require('express')
-let app = express()
+// required modules and variables
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const rowdy = require('rowdy-logger')
+const axios = require('axios')
+const morgan = require('morgan')
+const db = require('./models')
 
+const app = express()
+const rowdyResults = rowdy.begin(app)
+const PORT = process.env.PORT || 3000
+
+// middleware and config
 app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use(morgan('dev'))
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
+//controllers 
+
+// routes
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('layout')
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(PORT, () => {
+    rowdyResults.print()
+})
